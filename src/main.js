baf01b163778449b6f8ffd0c62a7c017865700e2
Atanas Keskovski <atanas.keskovski@students.finki.ukim.mk>
2026-03-03 13:47:14 +0100

    Second commit

--- src/main.js
+++ src/main.js
@@ -1,9 +1,8 @@
-// src/main.js
+
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 
-// ✅ MUST: load global CSS
 import './style.css'
 
 createApp(App).use(router).mount('#app')
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  base: 'https://github.com/atanaskeskovski/prikazni_za_deca'
 })
